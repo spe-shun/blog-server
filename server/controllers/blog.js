955d02fc4293fc5fd34a8e6f01fbd481a7d250b3
shunyu <yangshunyu@meituan.com>
2019-08-24 16:00:52 +0800

    test dir again

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -2,14 +2,22 @@ const fs = require('fs')
 const path = require('path')
 const MarkdownIt = require('markdown-it')
 
+const isDev = process.env.NODE_ENV === 'development'
+let rootDir
+console.log('isDev:', isDev)
+
+if (isDev) {
+  rootDir = `${process.cwd()}/resources`
+} else {
+  rootDir = '/usr/share/nginx/blog-server/resources'
+}
+
 const md = new MarkdownIt()
-const blogPath = path.resolve(__dirname, 'resources', 'blog')
-const picturePath = path.resolve(__dirname, 'resources', 'img')
 
 let pages = async ctx => {
   try {
-    console.log('file:', `${blogPath}/${ctx.params.name}.md`)
-    const data = fs.readFileSync(`${blogPath}/${ctx.params.name}.md`, 'utf-8')
+    console.log(rootDir + '/blog/' + ctx.params.name + '.md')
+    const data = fs.readFileSync(rootDir + '/blog/' + ctx.params.name + '.md', 'utf-8')
     ctx.response.body = md.render(data)
   } catch (error) {
     ctx.response.body = '<h1>404 NOT FOUND<h1/>'
@@ -20,7 +28,7 @@ let pages = async ctx => {
 
 let pageList = async ctx => {
   try {
-    const data = fs.readFileSync('./resources/lists.json', 'utf-8')
+    const data = fs.readFileSync(`${rootDir}/lists.json`, 'utf-8')
     ctx.response.body = data
   } catch (error) {
     ctx.response.body = '<h1>404 NOT FOUND<h1/>'
@@ -29,11 +37,8 @@ let pageList = async ctx => {
 
 let Img = async ctx => {
   try {
-    console.log(__dirname)
-    console.log(__filename)
-    console.log(process.cwd())
-    console.log('file:', `${picturePath}/${ctx.params.img}`)
-    const data = fs.readFileSync(`${picturePath}/${ctx.params.img}`)
+    console.log(rootDir + '/img/' + ctx.params.img)
+    const data = fs.readFileSync(rootDir + '/img/' + ctx.params.img)
     ctx.set('Access-Control-Allow-Origin', '*')
     ctx.response.body = data
   } catch (error) {
